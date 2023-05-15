ccec34734055225e1610e89c8df8ac223d2a5dc9
BearAndrew <a122333andrew@gmail.com>
2023-05-15 20:32:30 +0800

    init

---
+++ projects/my-lib/schematics/component-01/index.d.ts
@@ -0,0 +1,3 @@
+import { Rule } from '@angular-devkit/schematics';
+import { Component01 } from './schema';
+export declare function myService(options: Component01): Rule;
---
+++ projects/my-lib/schematics/component-01/schema.d.ts
@@ -0,0 +1,5 @@
+export interface Component01 {
+  name: string;
+  project?: string;
+  path?: string;
+}
