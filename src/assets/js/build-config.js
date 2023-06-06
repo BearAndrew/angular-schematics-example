f4fa16eae3675ec39ec327304a4f29975981a283
tomyang <tomyangtw@gmail.com>
2023-06-06 20:08:02 +0800

    feat: 依據config指定nginx位置

--- src/assets/js/build-config.js
+++ src/assets/js/build-config.js
@@ -7,9 +7,24 @@ const fileContent = fs.readFileSync('config.txt', 'utf-8');
 // 按行分割文件内容
 const commands = fileContent.split('\n');
 
+let dir = '';
+
 // 逐行执行指令
-commands.forEach((command) => {
-  if (command.trim() !== '') {
+commands.forEach((command, index) => {
+  if (index === 0) {
+    dir = command;
+  } else if (command.trim() !== '' && command.startsWith('ng')) {
     execSync(command.trim(), { stdio: 'inherit' });
   }
 });
+
+
+// 讀取檔案
+const filePath = 'src/appspec.yml';
+const appspecContent = fs.readFileSync(filePath, 'utf8');
+
+// 修改檔案內容
+const modifiedContent = appspecContent.replace('/etc/nginx', '/etc/nginx/' + dir);
+
+// 寫入修改後的內容
+fs.writeFileSync(filePath, modifiedContent, 'utf8');
