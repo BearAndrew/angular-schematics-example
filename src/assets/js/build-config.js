3a0a0252a4e3dca77d62cfa6d092eae7ad8f335d
tomyang <tomyangtw@gmail.com>
2023-06-19 18:39:32 +0800

    feat: delete user

--- src/assets/js/build-config.js
+++ src/assets/js/build-config.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra');
 const { execSync } = require('child_process');
 
-// 读取 myfile.txt 文件的内容
+// 讀取 config.txt 文件的内容
 const fileContent = fs.readFileSync('config.txt', 'utf-8');
 
 // 按行分割文件内容
@@ -9,22 +9,37 @@ const commands = fileContent.split('\n');
 
 let dir = '';
 
-// 逐行执行指令
-commands.forEach((command, index) => {
+// 設定行為：新增或刪除
+let action;
+
+// 逐行执行ng指令
+commands.some((command, index) => {
   if (index === 0) {
     dir = command;
+    action = dir.split(':')[1];
+    return !action;
   } else if (command.trim() !== '' && command.startsWith('ng')) {
     execSync(command.trim(), { stdio: 'inherit' });
   }
 });
-
 
 // 讀取檔案
 const filePath = 'src/appspec.yml';
 const appspecContent = fs.readFileSync(filePath, 'utf8');
 
 // 修改檔案內容
-const modifiedContent = appspecContent.replace('/etc/nginx/projects/', '/etc/nginx/projects/' + dir);
+let modifiedContent = appspecContent.replace('/etc/nginx/projects/', '/etc/nginx/projects/' + dir);
+
+// 判斷action是否為false, 是的話代表刪除該用戶資料及nginx設定
+if (action) {
+  //1.修改delete_user的檔案路徑
+  const scriptFilePath = 'src/delete_user';
+  const scriptContent = fs.readFileSync(scriptFilePath, 'utf8');
+  const newScriptContent = scriptContent.replace('/etc/nginx/sites-available/', '/etc/nginx/sites-available/' + dir);
+  fs.writeFileSync(scriptFilePath, newScriptContent, 'utf8');
+  //2.修改appspec.yml 要加入呼叫刪除的script片段
+  modifiedContent = modifiedContent.replace('start_server', 'delete_user');
+}
 
 // 寫入修改後的內容
 fs.writeFileSync(filePath, modifiedContent, 'utf8');
